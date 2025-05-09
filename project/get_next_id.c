2e43475361bdd2650ae2189e950a4024db58624e
S-Sarvesh <sarveshsundar2005@gmail.com>
2025-05-09 09:06:44 +0530

    updates

--- project/get_next_id.c
+++ project/get_next_id.c
@@ -1,27 +1,73 @@
-// File: get_next_id.cgi
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Node {
+    int id;
+    struct Node* next;
+} Node;
+
+void add_id(Node** head, int new_id) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->id = new_id;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+int id_exists(Node* head, int id) {
+    Node* current = head;
+    while (current != NULL) {
+        if (current->id == id) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
+void free_list(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
 int main() {
     FILE *fp = fopen("orders.txt", "r");
-    int maxId = 0;
+    Node* id_list = NULL;
+    int max_id = 0;
 
     if (fp != NULL) {
         char line[1024];
         while (fgets(line, sizeof(line), fp)) {
-            char id[100];
-            if (sscanf(line, "%[^|]", id) == 1) {
-                int num = atoi(id);
-                if (num > maxId) {
-                    maxId = num;
+            char id_str[100];
+            if (sscanf(line, "%[^|]", id_str) == 1) {
+                int current_id = atoi(id_str);
+                add_id(&id_list, current_id);
+                if (current_id > max_id) {
+                    max_id = current_id;
                 }
             }
         }
         fclose(fp);
     }
 
+    // Find the next available ID (could be max_id + 1 or a gap in the sequence)
+    int next_id = max_id + 1;
+    
+    // Optional: Check for gaps in the ID sequence to reuse deleted IDs
+    for (int i = 1; i < max_id; i++) {
+        if (!id_exists(id_list, i)) {
+            next_id = i;
+            break;
+        }
+    }
+
     printf("Content-Type: application/json\n\n");
-    printf("{\"nextOrderId\": \"%d\"}", maxId + 1);
+    printf("{\"nextOrderId\": \"%d\"}", next_id);
+
+    free_list(id_list);
     return 0;
 }
--- project/update_ingredient_stock.c
+++ project/update_ingredient_stock.c
@@ -2,53 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INGREDIENTS 10
-
-typedef struct {
+typedef struct IngredientNode {
     int id;
     char name[50];
     int stock;
     int lowStockThreshold;
-} Ingredient;
+    struct IngredientNode* next;
+} IngredientNode;
 
-Ingredient ingredients[MAX_INGREDIENTS] = {
-    {1, "Flour", 50, 10},
-    {2, "Sugar", 20, 5},
-    {3, "Eggs", 100, 20},
-    {4, "Salt", 15, 5},
-    {5, "Butter", 30, 5},
-    {6, "Milk", 10, 3},
-    {7, "Cheese", 40, 10},
-    {8, "Tomato", 25, 10},
-    {9, "Lettuce", 12, 3},
-    {10, "Chicken", 60, 15}
-};
+// Global head pointer for the linked list
+IngredientNode* ingredients_head = NULL;
 
+// Function to initialize the ingredients linked list
+void initialize_ingredients() {
+    // Create initial ingredients
+    IngredientNode* initial_ingredients[] = {
+        &(IngredientNode){1, "Flour", 50, 10, NULL},
+        &(IngredientNode){2, "Sugar", 20, 5, NULL},
+        &(IngredientNode){3, "Eggs", 100, 20, NULL},
+        &(IngredientNode){4, "Salt", 15, 5, NULL},
+        &(IngredientNode){5, "Butter", 30, 5, NULL},
+        &(IngredientNode){6, "Milk", 10, 3, NULL},
+        &(IngredientNode){7, "Cheese", 40, 10, NULL},
+        &(IngredientNode){8, "Tomato", 25, 10, NULL},
+        &(IngredientNode){9, "Lettuce", 12, 3, NULL},
+        &(IngredientNode){10, "Chicken", 60, 15, NULL}
+    };
+    
+    // Link them together
+    for (int i = 0; i < 9; i++) {
+        initial_ingredients[i]->next = initial_ingredients[i+1];
+    }
+    
+    ingredients_head = initial_ingredients[0];
+}
+
+// Function to update stock for an ingredient
 void update_stock(int ingredient_id, int new_stock) {
-    for (int i = 0; i < MAX_INGREDIENTS; i++) {
-        if (ingredients[i].id == ingredient_id) {
-            ingredients[i].stock = new_stock;
-            break;
+    IngredientNode* current = ingredients_head;
+    while (current != NULL) {
+        if (current->id == ingredient_id) {
+            current->stock = new_stock;
+            return;
         }
+        current = current->next;
     }
 }
 
+// Function to free the linked list
+void free_ingredients() {
+    IngredientNode* current = ingredients_head;
+    while (current != NULL) {
+        IngredientNode* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    ingredients_head = NULL;
+}
+
 int main() {
+    // Initialize the ingredients linked list
+    initialize_ingredients();
+    
     char *data;
     int ingredient_id, new_stock;
     
     // Get the data from the POST request
     data = getenv("QUERY_STRING");
     if (data != NULL) {
-        sscanf(data, "ingredient_id=%d&new_stock=%d", &ingredient_id, &new_stock);
-        update_stock(ingredient_id, new_stock);
-        
-        printf("Content-Type: text/plain\n\n");
-        printf("Stock updated successfully!\n");
+        if (sscanf(data, "ingredient_id=%d&new_stock=%d", &ingredient_id, &new_stock) == 2) {
+            update_stock(ingredient_id, new_stock);
+            
+            printf("Content-Type: text/plain\n\n");
+            printf("Stock updated successfully!\n");
+        } else {
+            printf("Content-Type: text/plain\n\n");
+            printf("Error: Invalid data format.\n");
+        }
     } else {
         printf("Content-Type: text/plain\n\n");
         printf("Error: No data received.\n");
     }
+    
+    // Free the linked list
+    free_ingredients();
     
     return 0;
 }
